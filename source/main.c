902a8bc8183bc18b304d02469350044958542d45
LadyNick <72218995+LadyNick@users.noreply.github.com>
2021-02-28 10:43:25 -0800

    Part 1 check

--- source/main.c
+++ source/main.c
@@ -14,55 +14,82 @@
 #include "scheduler.h"
 #endif
 
-
+unsigned char pattern = 0xFF;
+unsigned char row = 0xFE;
+unsigned char A1;
+unsigned char A0;
 //--------------------------------------
-// LED Matrix Demo SynchSM
+// LED Row Shift SynchSM
 // Period: 100 ms
 //--------------------------------------
-enum Demo_States {shift};
-int Demo_Tick(int state) {
-
-	// Local Variables
-	static unsigned char pattern = 0x80;	// LED pattern - 0: LED off; 1: LED on
-	static unsigned char row = 0xFE;  	// Row(s) displaying pattern. 
-							// 0: display pattern on row
-							// 1: do NOT display pattern on row
+enum Row_States {wait, rowup,rowdown, rowrelease}Row_State;
+int Row_Tick(int Row_State) {
+	
 	// Transitions
-	switch (state) {
-		case shift:	
-break;
-		default:	
-state = shift;
-			break;
-	}	
-	// Actions
-	switch (state) {
-case shift:	
-if (row == 0xEF && pattern == 0x01) { // Reset demo 
-				pattern = 0x80;		    
-				row = 0xFE;
-			} else if (pattern == 0x01) { // Move LED to start of next row
-				pattern = 0x80;
-				row = (row << 1) | 0x01;
-			} else { // Shift LED one spot to the right on current row
-				pattern >>= 1;
+	switch (Row_State) {
+		case wait:
+			if(A0){
+				Row_State = rowup;
+			}
+			else if(A1){
+				Row_State = rowdown;
+			}
+			else{
+				Row_State = wait;
 			}
 			break;
-		default:
-	break;
+		case rowdown:
+			if(row == 0xEF){
+				//do nothing, keep it the same
+			}
+			else{
+				row = (row << 1) | 0x01;
+			}
+			Row_State = rowrelease;
+			break;
+		case rowdown:
+			if(row == 0xFE){
+				//do nothing, keep it the same
+			}
+			else{
+				row = (row >> 1) | 0x80;
+			}
+			Row_State = rowrelease;
+			break;
+		case rowrelease:
+			if(!A0 && !A1){
+				Row_State = wait;
+			}
+			else{
+				Row_State = rowrelease;
+			}
+			break;
+		default: Row_State = wait;
 	}
-	PORTC = pattern;	// Pattern to display
-	PORTD = row;		// Row(s) displaying pattern	
-	return state;
 }
+
+enum Display_States{display}Display_State;
+Display_Tick(int Display_State){
+	switch(Display_State){
+		case display:
+			PORTC = pattern;
+			PORTD = row;
+			Display_State = display;
+			break;
+		default: Display_State = display; break;
+	}
+}
+
+
 
 
 int main(void) {
     DDRC = 0xFF; PORTC = 0x00;
     DDRD = 0xFF; PORTD = 0x00;
+    DDRA = 0x00; PINA = 0xFF;
 
-    static task task1;
-    task *tasks[] = {&task1};
+    static task task1, task2;
+    task *tasks[] = {&task1, &task2};
     const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
     const char start = -1;
@@ -71,11 +98,18 @@ int main(void) {
     task1.state = start;
     task1.period = 100; 
     task1.elapsedTime = task1.period;
-    task1.TickFct = &Demo_Tick;
+    task1.TickFct = &Row_Tick;
+	
+    task2.state = start;
+    task2.period = 50;
+    task2.elapsedTime = task2.period;
+    task2.TickFct = &Display_Tick;
 
     TimerSet(1);
     TimerOn();
     unsigned short i;
+	A0 = ~PINA & 0x01;
+	A1 = ~PINA & 0x02;
 
     while (1) {
 	    for(i=0; i<numTasks; i++){ //Scheduler code
