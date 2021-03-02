0a3ebc26a46a54cc6294390f2b2549a9009e6d64
LadyNick <72218995+LadyNick@users.noreply.github.com>
2021-03-01 21:44:28 -0800

    Update main.c

--- source/main.c
+++ source/main.c
@@ -27,47 +27,36 @@ unsigned char A1;
 unsigned char A2;
 unsigned char A3;
 
-void transmit_sreg1(unsigned char data) {
-    int i;
-    for (i = 0; i < 8 ; ++i) {
-   	 // Sets SRCLR to 1 allowing data to be set
-   	 // Also clears SRCLK in preparation of sending data
-	    
-	 	PORTC = 0x08;
-	
-   	 // set SER = next bit of data to be sent.
-   	 PORTC |= ((data >> i) & 0x01);
-   	 // set SRCLK = 1. Rising edge shifts next bit of data into the shift register
-   	 PORTC |= 0x02;
-    }
-    // set RCLK = 1. Rising edge copies data from “Shift” register to “Storage” register
+void transmit_data(unsigned char data, unsigned char reg) {
 
-	PORTC |= 0x04;
-	
-    // clears all lines in preparation of a new transmission
-    PORTC = 0x00;
-}
-
-void transmit_sreg2(unsigned char data) {
     int i;
-    for (i = 0; i < 8 ; ++i) {
-   	 // Sets SRCLR to 1 allowing data to be set
-   	 // Also clears SRCLK in preparation of sending data
-	 
-	 	PORTD = 0x08;
-	 
-	
-   	 // set SER = next bit of data to be sent.
-   	 PORTD |= ((data >> i) & 0x01);
-   	 // set SRCLK = 1. Rising edge shifts next bit of data into the shift register
-   	 PORTD |= 0x02;
+    if (reg == 1) {
+        for (i = 0; i < 8 ; ++i) {
+        // Sets SRCLR to 1 allowing data to be set
+        // Also clears SRCLK in preparation of sending data
+        PORTC = 0x08;
+            // set SER = next bit of data to be sent.
+            PORTC |= ((data >> i) & 0x01);
+            // set SRCLK = 1. Rising edge shifts next bit of data into the shift register
+            PORTC |= 0x02;  
+        }
+        // set RCLK = 1. 
+        PORTC |= 0x04;
     }
-    // set RCLK = 1. Rising edge copies data from “Shift” register to “Storage” register
- 
-	PORTD |= 0x04;
-    
-    // clears all lines in preparation of a new transmission
-    PORTD = 0x00;
+
+    else if (reg == 2) {
+        for (i = 0; i < 8 ; ++i) {
+        // Sets SRCLR to 1 allowing data to be set
+        // Also clears SRCLK in preparation of sending data
+        PORTD = 0x08;
+            // set SER = next bit of data to be sent.
+            PORTD |= ((data >> i) & 0x01);
+            // set SRCLK = 1. Rising edge shifts next bit of data into the shift register
+            PORTD |= 0x02;  
+        }
+        // set RCLK = 1. 
+        PORTD |= 0x04;
+    }
 }
 
 enum Move_States {wait, up, down, left, right, release}Move_State;
@@ -176,8 +165,8 @@ int Display_Tick(int Display_State){
 	switch(Display_State){
 
 		case display:
-			transmit_sreg1(0xFF/*pattern[update]*/);
-			transmit_sreg2(0xFE/*row[update]*/);
+			transmit_data(pattern[update]);
+			transmit_data(row[update]);
 			++update;
 			if(update > 4){
 				update = 0;
